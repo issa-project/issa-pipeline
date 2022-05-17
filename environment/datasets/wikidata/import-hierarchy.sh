5dac1ff756be28c301de1194e96bb696c42dfdb2
Anna Bobasheva <33026767+AnnaBobasheva@users.noreply.github.com>
2022-05-17 17:40:23 +0200

    Added Wikidata hierachy fetching functionality

---
+++ environment/datasets/wikidata/import-hierarchy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Author: Anna BOBASHEVA, University Cote d'Azur, Inria
+#
+# Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+# Load Wikidatad lables and hierarchy into Virtuoso triplestore
+
+# ISSA environment definitions
+. ../../../env.sh
+
+log_dir=../../logs
+mkdir -p $log_dir 
+log=$log_dir/wikidata_import_$(date "+%Y%m%d_%H%M%S").log
+
+docker start virtuoso
+
+# TODO: move to env.sh
+WIKIDATA_IMPORT_DIR=~/ISSA/volumes/virtuoso/database/import
+
+cp -v wikidata-dump-en.ttl "$WIKIDATA_IMPORT_DIR"              >>$log
+cp -v import-hierarchy.isql "$WIKIDATA_IMPORT_DIR"              >>$log
+
+docker exec virtuoso \
+            isql -H localhost -U dba -P $VIRTUOSO_PWD \
+            exec="LOAD /database/import/import-hierarchy.isql"    &>>$log
+
+
+
