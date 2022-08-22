9bf3b1814c447c5ea0e752fd48fb819b43ac0578
Anna Bobasheva <33026767+AnnaBobasheva@users.noreply.github.com>
2022-08-22 15:01:56 +0200

    Update env.sh with explanations

--- env.sh
+++ env.sh
@@ -3,45 +3,30 @@
 #
 # Tune the variables in this file according to the environment
 
-# Agrovoc dump
-AGROVOC_URL=https://agrovoc.fao.org/latestAgrovoc/agrovoc_core.nt.zip
-AGROVOC_GRAPH=http://agrovoc.fao.org/graph
-AGROVOC_IMPORT_DIR=~/ISSA/volumes/virtuoso/database/import
-
-# GeoNames dump
-GEONAMES_DUMP_URL=https://download.geonames.org/all-geonames-rdf.zip
-GEONAMES_GRAPH=http://geonames.org/graph 
-GEONAMES_IMPORT_DIR=~/ISSA/volumes/virtuoso/database/import
-
 # ISSA version (dot- and dashed-notation)
-ISSA_VERSION=1.0
-ISSA_VERSION_DASH=1-0
-ISSA_DATASET_NAME=issa-agritrop
 
-# ISSA dataset id 
-export ISSA_DATA_ROOT=~/ISSA/data
-export ISSA_DATASET=dataset-${ISSA_VERSION_DASH}
+# ISSA dataset nomenclature
+ISSA_VERSION=1.1                             # version with dots 
+ISSA_VERSION_DASH=1-1                        # version with dashes
 
+export ISSA_DATA_ROOT=~/ISSA/data            # root of all data files
+export ISSA_DATASET=dataset-${ISSA_VERSION_DASH} 
+ISSA_DATASET_NAME=issa-agritrop              # this dataset name is used in the RDF dataset definition 
+
+# Find the latest update folder 
+# The latest update folder is created when the update repositry is created in create_repository.py
 mkdir -p $ISSA_DATA_ROOT/$ISSA_DATASET
-export LATEST_UPDATE=$(ls -Ar  --group-directories-first $ISSA_DATA_ROOT/$ISSA_DATASET | head -n 1) # doesn't work if the $ISSA_DATA_ROOT/$ISSA_DATASET does not exsist
+export LATEST_UPDATE=$(ls -Ar  --group-directories-first $ISSA_DATA_ROOT/$ISSA_DATASET | head -n 1) 
 export LATEST_UPDATE_DIR=$ISSA_DATA_ROOT/$ISSA_DATASET/$LATEST_UPDATE
 
-# potentially use this var to create a dataset version
-#CURRENT_DATE=$(date "+%Y-%m-%d") 
-
-export METADATA_PREFIX=agritrop_meta
-
-# PDF storage
-export PDF_CACHE=~/ISSA/data/pdf_cache 
-export PDF_CACHE_UNREADBLE=$PDF_CACHE/unreadable
-
-# Directories of data files relative to LATEST_UPDATE_DIR
+# Directories of data files relative to the LATEST_UPDATE_DIR
 # - dirs used downstream in the pipeline
-export REL_META=.    					# metadata (tsv) 
-export REL_FULLTEXT=json/coalesced		# Grobid extracted data (json) 
-export REL_SPOTLIGHT=annotation/dbpedia	# DBpedia Spotlight annotations
-export REL_EF=annotation/wikidata		# Entity-Fishing annotations 
-export REL_GEONAMES=annotation/geonames	# GeoNames annotations 
+export REL_META=.                            # metadata (tsv) 
+export REL_FULLTEXT=json/coalesced	          # Grobid extracted data (json) 
+export REL_SPOTLIGHT=annotation/dbpedia	     # DBpedia Spotlight annotations
+export REL_EF=annotation/wikidata            # Entity-Fishing annotations 
+export REL_GEONAMES=annotation/geonames	     # GeoNames annotations
+export REL_PYCLINREC=annotation/agrovoc      # Agrovoc annotations (use case specific vocabulary) 
 export REL_ANNIF=indexing				# Relative directory of Annif output
 export REL_RDF=rdf						# Relative directory of RDF output
 
@@ -50,69 +35,93 @@ export REL_ANNIF_LABELS=lables			# label tsv files for Annif training
 export REL_ANNIF_TEXT=txt				# text files that can be used for ANNIF trainig
 export REL_PDF=pdf						# document pdfs
 export REL_GROBID_XML=xml				# Grobid extracted data (xml)
-export REL_META_JSON=json/metadata		# text contained in metadata formated as json 
-export REL_GROBID_JSON=json/fulltext	# text extracted by Grobid formated as json
+export REL_META_JSON=json/metadata		     # text contained in metadata formated as json 
+export REL_GROBID_JSON=json/fulltext	     # text extracted by Grobid formated as json
 export REL_COAL_JSON=json/coalesced		# json coalesced from the two above with metadata replacing Grobit when present 
-                               
 
 # Python virtual environment name and location
 ISSA_VENV=~/ISSA/environment/python/issa_venv
 export ISSA_SRC_ROOT=~/ISSA/pipeline 
 
+# Metadata configuration
+export METADATA_PREFIX=agritrop_meta         # arbitrary metadata file name
 
-# MongoDB 
-MONGODB_HOST_DATABASE_DIR=~/ISSA/volumes/mongodb # maps to /data/db
-MONGODB_HOST_DATA_DIR=$ISSA_DATA_ROOT
-MONGODB_CONT_DATA_DIR=/issa/data
-MONGODB_HOST_SCRIPT_DIR=$ISSA_SRC_ROOT/mongo
-MONGODB_CONT_SCRIPT_DIR=/issa/script
-MONGODB_IMPORT_DIR=$MONGODB_CONT_DATA_DIR/$ISSA_DATASET/$LATEST_UPDATE
-MONGODB_DB=$ISSA_DATASET-$LATEST_UPDATE
+# PDF storage
+export PDF_CACHE=~/ISSA/data/pdf_cache           # location for PDF cache
+export PDF_CACHE_UNREADBLE=$PDF_CACHE/unreadable # separate the unreadables by Grobid here
+
+# MongoDB (docker container)
+MONGODB_CONT_NAME=mongodb                        # docker container name 
+MONGODB_DB=$ISSA_DATASET-$LATEST_UPDATE          # each data update is stored in a separate database in Mongo 
+MONGODB_HOST_DATABASE_DIR=~/ISSA/volumes/mongodb # map to /data/db in the container FS for data persistency 
+MONGODB_HOST_DATA_DIR=$ISSA_DATA_ROOT            # map host data dir
+MONGODB_CONT_DATA_DIR=/issa/data                 # to container's FS to access data on host
+MONGODB_HOST_SCRIPT_DIR=$ISSA_SRC_ROOT/mongo     # map host script dir
+MONGODB_CONT_SCRIPT_DIR=/issa/script             # to container's FS to access scripts that have to be executed in the container
+MONGODB_IMPORT_DIR=$MONGODB_CONT_DATA_DIR/$ISSA_DATASET/$LATEST_UPDATE    # path to the latest files to import
 
 # xR2RML tool
-XR2RML_TRANSFORMATION_DIR=$ISSA_SRC_ROOT/xR2RML
-XR2RML_OUTPUT_DIR=$LATEST_UPDATE_DIR/$REL_RDF
+XR2RML_TRANSFORMATION_DIR=$ISSA_SRC_ROOT/xR2RML  # tool location   
+XR2RML_OUTPUT_DIR=$LATEST_UPDATE_DIR/$REL_RDF    # output for generated RDF files
 
-# Virtuoso 
-VIRTUOSO_DATABASE_DIR=~/ISSA/volumes/virtuoso/database
-VIRTUOSO_DEAFAULT_GRAPH=http://data-issa.cirad.fr/graph
-#####VIRTUOSO_HOST_DATA_DIR=/home/issa/ISSA/data/dataset-0-0/en/$REL_RDF 
-VIRTUOSO_HOST_DATA_DIR=$ISSA_DATA_ROOT
-VIRTUOSO_CONT_DATA_DIR=/issa/data                        #IMPORTANT: The same dir has to be added to DirsAllowed in virtuoso.ini
-VIRTUOSO_HOST_SCRIPT_DIR=$ISSA_SRC_ROOT/virtuoso
-VIRTUOSO_CONT_SCRIPT_DIR=/issa/script
-VIRTUOSO_IMPORT_DIR=$VIRTUOSO_CONT_DATA_DIR/$ISSA_DATASET/$LATEST_UPDATE/$REL_RDF
+# Virtuoso (docker container)
+VIRTUOSO_CONT_NAME=virtuoso                     # docker container name 
+VIRTUOSO_DATABASE_DIR=~/ISSA/volumes/virtuoso/database    # map to /database in the container FS for data presistency    
+VIRTUOSO_DEAFAULT_GRAPH=http://data-issa.cirad.fr/graph   # arbitrary default graph name
+VIRTUOSO_HOST_DATA_DIR=$ISSA_DATA_ROOT           # map host data dir
+VIRTUOSO_CONT_DATA_DIR=/issa/data                # to container's FS to access data on host. IMPORTANT: The same dir has to be added to DirsAllowed in virtuoso.ini
+VIRTUOSO_HOST_SCRIPT_DIR=$ISSA_SRC_ROOT/virtuoso # map host script dir 
+VIRTUOSO_CONT_SCRIPT_DIR=/issa/script            # to container's FS to access scripts that have to be executed in the container 
+VIRTUOSO_IMPORT_DIR=$VIRTUOSO_CONT_DATA_DIR/$ISSA_DATASET/$LATEST_UPDATE/$REL_RDF # path to the latest files to import
 
 
-# Annif
-ANNIF_IMAGE=issa
-ANNIF_PROJECTS_DIR=~/ISSA/volumes/annif-projects
-ANNIF_HOST_DATA_DIR=$ISSA_DATA_ROOT
-ANNIF_CONT_DATA_DIR=/issa/data
-ANNIF_TRAINING_DIR=$ISSA_DATA_ROOT/training
-ANNIF_INPUT_DIR=$ANNIF_CONT_DATA_DIR/$ISSA_DATASET/$LATEST_UPDATE/indexing
-ANNIF_PROJECT=cirad-nn-ensemble
-export ANNIF_SUFFIX=.parabel_mllm_nn
-ANNIF_LIMIT=15
-ANNIF_THRESHOLD=0.1
-ANNIF_LANGUAGES=en\ fr
+# Annif (docker container)
+ANNIF_IMAGE=issa                                 # obsolete 
+ANNIF_CONT_NAME=annif                            # docker container name
+ANNIF_PROJECTS_DIR=~/ISSA/volumes/annif-projects # map to /annif-projects in the container FS for data presistency
+ANNIF_HOST_DATA_DIR=$ISSA_DATA_ROOT              # map host data dir
+ANNIF_CONT_DATA_DIR=/issa/data                   # to container's FS to access data on host.
+ANNIF_TRAINING_DIR=$ISSA_DATA_ROOT/training      # dir for Annif training data
+ANNIF_INPUT_DIR=$ANNIF_CONT_DATA_DIR/$ISSA_DATASET/$LATEST_UPDATE/indexing # path to the latest files to process
+ANNIF_PROJECT=cirad-nn-ensemble                  # name of the best model to use in indexing
+export ANNIF_SUFFIX=.parabel_mllm_nn             # suffix to add to the indexing output file name and should correspond to the previous var
+ANNIF_LIMIT=15                                   # max number of returned descriptors
+ANNIF_THRESHOLD=0.1                              # confidence threshold of  returned descriptors
+ANNIF_LANGUAGES=en\ fr                           # list of languages to process
 
-# DBPedia-Spotlight
-SPOTLIGHT_MODELS_DIR=~/ISSA/volumes/spotlight/models
-SPOTLIGHT_LANGUAGES=en\ fr
+# DBPedia-Spotlight (docker container)
+SPOTLIGHT_CONT_NAME=dbpedia-spotlight                # docker container name
+SPOTLIGHT_MODELS_DIR=~/ISSA/volumes/spotlight/models # map to /opt/spotlight/models in the container FS for data presistency
+SPOTLIGHT_LANGUAGES=en\ fr                           # list of available language models
 
-# Wikidata Entity-Fishing
-EF_MODELS_DIR=~/ISSA/volumes/entity-fishing/models
-EF_LANGUAGES=en\ fr
+# Wikidata Entity-Fishing (docker container)
+EF_CONT_NAME=entity-fishing                          # docker container name
+EF_MODELS_DIR=~/ISSA/volumes/entity-fishing/models   # map to /opt/entity-fishing/data/db in the container FS for data presistency
+EF_LANGUAGES=en\ fr                                  # list of available language models
 
-# Dataset
-DATASET_META_IMPORT_DIR=~/ISSA/volumes/virtuoso/database/import
+# Pyclinrec concept recognizer with Agrovoc vocabulary (docker container)
+PYCLINREC_CONT_NAME=agrovoc-pyclinrec                        # docker container name
+PYCLINREC_DICT_ENDPOINT=https://data-issa.cirad.fr/sparql    # endpoint to SKOS vocabulary to create concept dictionary
+PYCLINREC_DICT_GRAPH=http://agrovoc.fao.org/graph            # endpoint graph name to restrict a vocabulary
+PYCLINREC_HOST_CACHE=~/ISSA/volumes/agrovoc-pyclinrec/cache  # map to /app/cache dit on the conatainer FS for dictionaries and recognizer objects presistency
 
-# Lables and hierarchies 
-WIKIDATA_IMPORT_DIR=~/ISSA/volumes/virtuoso/database/import
-DBPEDIA_IMPORT_DIR=~/ISSA/volumes/virtuoso/database/import
+# Dataset metadata
+DATASET_META_IMPORT_DIR=$VIRTUOSO_DATABASE_DIR/import # copy dataset metadata RDF files and import scripts to this dir for upload 
 
-# Agrovoc-Pyclinrec concept recognizer
-PYCLINREC_DICT_ENDPOINT=https://data-issa.cirad.fr/sparql
-PYCLINREC_DICT_GRAPH=http://agrovoc.fao.org/graph
-PYCLINREC_HOST_CACHE=~/ISSA/volumes/agrovoc-pyclinrec/cache
+# External datasets import
+
+# Agrovoc dump
+AGROVOC_URL=https://agrovoc.fao.org/latestAgrovoc/agrovoc_core.nt.zip # URL for Agrovoc RDF data dump
+AGROVOC_GRAPH=http://agrovoc.fao.org/graph 		   # graph name for Agrovoc triples
+AGROVOC_IMPORT_DIR=$VIRTUOSO_DATABASE_DIR/import     # copy Agrovoc RDF dump and import scripts to this dir for upload 
+
+# GeoNames dump
+GEONAMES_DUMP_URL=https://download.geonames.org/all-geonames-rdf.zip # URL for Geonames RDF data dump
+GEONAMES_GRAPH=http://geonames.org/graph             # graph name for GeoNames triples
+GEONAMES_IMPORT_DIR=$VIRTUOSO_DATABASE_DIR/import    # copy geonames RDF dump and import scripts to this dir for upload 
+
+# Wikidata labels and hierarchies dump
+WIKIDATA_IMPORT_DIR=$VIRTUOSO_DATABASE_DIR/import    # copy wikidata RDF dump and import scripts to this dir for upload 
+
+# DBpedia labels and hierarchies dump
+DBPEDIA_IMPORT_DIR=$VIRTUOSO_DATABASE_DIR/import     # copy DBpedia RDF dump and import scripts to this dir for upload 
