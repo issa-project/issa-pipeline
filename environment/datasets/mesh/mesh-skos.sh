f1b2898e1ebc60df9bb23cd4814577fbb93b8444
Anna Bobasheva <33026767+AnnaBobasheva@users.noreply.github.com>
2023-12-02 18:27:08 +0100

    Added scripts to insert SKOS triiples to MeSH graph

--- environment/datasets/mesh/mesh-skos.sh
+++ environment/datasets/mesh/mesh-skos.sh
@@ -2,24 +2,22 @@
 # Author: Anna BOBASHEVA, University Cote d'Azur, Inria
 #
 # Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
-# Load Agrovoc thesaurus into Virtuoso triplestore
+#
+# Update dataset metadata such as the number of triples and dates
 
 # ISSA environment definitions
-. ../../../env.sh
+. ../../env.sh
 
 log_dir=${ISSA_ENV_LOG:-../../logs}
 mkdir -p $log_dir 
-log=$log_dir/import_mesh_$(date "+%Y%m%d_%H%M%S").log
+log=$log_dir/insert_mesh_skos_$(date "+%Y%m%d_%H%M%S").log
 
 VIRTUOSO=${VIRTUOSO_CONT_NAME:-virtuoso}
 
 docker start $VIRTUOSO
 
-cp -v import-mesh.isql "$MESH_IMPORT_DIR"                      >>$log
+cp -v mesh-skos.isql "$MESH_IMPORT_DIR"                      >>$log
 
 docker exec $VIRTUOSO \
             isql -H localhost -U dba -P $VIRTUOSO_PWD \
-            exec="LOAD /database/import/import-mesh.isql"      &>>$log
-
-
-
+            exec="LOAD /database/import/mesh-skos.isql"      &>>$log
